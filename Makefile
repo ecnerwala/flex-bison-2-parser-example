9a6bd7cf8ab046c633cc61d84aa3be57a45f9176
Andrew He <andrewhe@mit.edu>
2017-03-25 22:09:02 -0400

    Create a copy of parser.yy and lexer.lex, and update the support code.

--- Makefile
+++ Makefile
@@ -1,11 +1,11 @@
-all: parser.cpp lexer.cpp bc-parser.cpp
-	g++ -g -std=c++1y bc-parser.cpp parser.cpp lexer.cpp -o bc-parser
+all: parser.cpp lexer.cpp bc-parser.cpp parser2.cpp lexer2.cpp
+	g++ -g -std=c++1y bc-parser.cpp parser.cpp lexer.cpp parser2.cpp lexer2.cpp -o bc-parser
 
-parser.cpp: parser.yy
-	bison --output=parser.cpp --defines=parser.h -v parser.yy
+%.cpp: %.yy
+	bison --output=$*.cpp --defines=$*.h -v $*.yy
 
-lexer.cpp: lexer.lex
-	flex  --outfile=lexer.cpp --header-file=lexer.h lexer.lex
+%.cpp: %.lex
+	flex  --outfile=$*.cpp --header-file=$*.h $*.lex
 
 clean: 
-	rm -f lexer.cpp lexer.h parser.cpp parser.h parser.output bc-parser
+	rm -f lexer.cpp lexer.h parser.cpp parser.h parser.output lexer2.cpp lexer2.h parser2.cpp parser2.h parser2.output bc-parser
--- bc-parser.cpp
+++ bc-parser.cpp
@@ -1,6 +1,8 @@
 
 #include "parser.h"
 #include "lexer.h"
+#include "parser2.h"
+#include "lexer2.h"
 #include <iostream>
 #include "prettyprinter.h"
 
@@ -8,16 +10,31 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-  void* scanner;
-  yylex_init(&scanner);
-  yyset_in(stdin, scanner);
-  
   Function* output;
-  int rvalue = yyparse(scanner, output);
-  if(rvalue == 1){
+  if (argc == 1) {
+    cout<<"Using parser"<<endl;
+    void* scanner;
+    yylex_init(&scanner);
+    yyset_in(stdin, scanner);
 
-	  cout<<"Parsing failed"<<endl;
-	  return 1;
+    int rvalue = yyparse(scanner, output);
+    if(rvalue == 1){
+
+      cout<<"Parsing failed"<<endl;
+      return 1;
+    }
+  } else {
+    void* scanner;
+    cout<<"Using parser2"<<endl;
+    yylex_init(&scanner);
+    yyset_in(stdin, scanner);
+
+    int rvalue = yyparse(scanner, output);
+    if(rvalue == 1){
+
+      cout<<"Parsing failed"<<endl;
+      return 1;
+    }
   }
 
   std::shared_ptr<Function> func(output);
